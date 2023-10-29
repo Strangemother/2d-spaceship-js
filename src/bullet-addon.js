6bcb948cba305a9fc62038b1c2ba8effe8dedc81
Jay Jagpal <jay@strangemother.com>
2023-10-29 23:09:29 +0000

    Multi Blaster

--- src/bullet-addon.js
+++ src/bullet-addon.js
@@ -5,14 +5,14 @@ class Bullet {
         this.x = x;
         this.y = y;
         this.direction = direction;
-        this.velocity = 5; // Adjust as needed
+        this.velocity = 3; // Adjust as needed
         this.element = el
-        console.log('Bullet', x,y)
+        // console.log('Bullet', x,y)
         this.t = undefined
     }
 
     updatePosition(t) {
-        console.log('tick')
+        // console.log('tick')
         if(this.t == undefined) {
             this.t = t
         }
@@ -22,9 +22,18 @@ class Bullet {
         }
 
         let vhp = this.direction * (Math.PI / 180)
-        let x = this.x + Math.sin(vhp) * this.velocity;
-        let y = this.y - Math.cos(vhp) * this.velocity;
-
+        let travel = false;
+        
+        let x = this.x
+        if(travel) {
+            x += Math.sin(vhp) * this.velocity;
+        }
+        
+        let y = this.y 
+        if(travel) {
+            y -= Math.cos(vhp) * this.velocity;
+        }
+        
         let xy = gridWrap(
                 x,y
             )
@@ -46,9 +55,39 @@ class Bullet {
     }
 
     kill(){
-        console.log('Kill bullet')
+        // console.log('Kill bullet')
         let bullet = bulletBox.remove(this)
         this.element.parentElement.removeChild(this.element)
+    }
+}
+
+
+class Blaster {
+    constructor(position, direction) {
+        this.position = position; // Relative position to the ship's center { x: 0, y: 0 }
+        this.direction = direction; // Direction in which the blaster fires bullets (in degrees)
+    }
+
+    fire(shipRotation, shipPosition, shipSize) {
+        // Calculate the effective direction of the bullet considering the ship's rotation
+        const effectiveDirection = this.direction + shipRotation;
+
+        // Calculate the starting position of the bullet considering the ship's position and the blaster's relative position
+
+        // let bulletX = shipPosition.x + this.position.x;
+        // let bulletY = shipPosition.y + this.position.y;
+        // bulletX = shipPosition.x + (shipSize.width / 2) + this.position.x;
+        // bulletY = shipPosition.y + (shipSize.height / 2) + this.position.y;
+
+        // Rotate the blaster's position based on the ship's rotation
+        const rotatedPosition = rotatePoint(this.position, shipRotation);
+
+        // Calculate the starting position of the bullet considering the ship's position,
+        // the rotated blaster's position, and the ship's size
+        const bulletX = shipPosition.x + shipSize.width / 2 + rotatedPosition.x;
+        const bulletY = shipPosition.y + shipSize.height / 2 + rotatedPosition.y;
+
+        return bulletBox.create(bulletX, bulletY, effectiveDirection)
     }
 }
 
@@ -62,7 +101,7 @@ class BulletBox {
         let bc = this.bullets.length
         this.bullets = this.bullets.filter(item => item !== bullet)
         let ba = this.bullets.length
-        console.log('Bullet removal count:', bc - ba)
+        // console.log('Bullet removal count:', bc - ba)
     }
 
     create(x,y, direction) {
@@ -84,21 +123,53 @@ class BulletBox {
 }
 
 
+function rotatePoint(point, angle) {
+    const radianAngle = angle * (Math.PI / 180);
+    const cosAngle = Math.cos(radianAngle);
+    const sinAngle = Math.sin(radianAngle);
+    const offsetY = 5
+    const offsetX = 5
+    return {
+        x: (point.x * cosAngle - point.y * sinAngle) + offsetX,
+        y: (point.x * sinAngle + point.y * cosAngle) + offsetY
+    };
+}
+
+
 const bulletBox = new BulletBox()
 
 
-class ShooterShip extends MotionShip {
+class BlasterShip extends MotionShip {
     // ... existing code ...
 
     constructor(element) {
         super(element);
         // ... existing code ...
         this.bullets = [];
+        // Example: Blaster positioned above the ship's center and fires upwards
+        this.blasters = [
+            // new Blaster({ x: -30, y: 0 }, 0),
+            new Blaster({ x: 50, y: 50 }, 45),
+            new Blaster({ x: -50, y: -50 }, 0),
+            // new Blaster({ x: 30, y: 0 }, 90),
+            // new Blaster({ x: 30, y: 0 }, 180),
+        ]
+
     }
 
     fireBullet() {
-        console.log('Pew!')
-        bulletBox.create(this.x, this.y, this.rotationAngle)
+        // console.log('Pew!')
+        const shipSize = {
+            width: this.element.offsetWidth,
+            height: this.element.offsetHeight
+        };
+        for(let blaster of this.blasters) {
+            const bullet = blaster.fire(this.rotationAngle, { x: this.x, y: this.y }, shipSize);
+            this.bullets.push(bullet);
+        }
+        // bulletBox.create(this.x, this.y, this.rotationAngle)
+        // this.blaster.fire(this.rotationAngle, {x: this.x, y: this.y}, shipSize)
+        // bulletBox.create(this.x, this.y, this.rotationAngle)
     }
 
     tickChange(t) {
@@ -109,7 +180,7 @@ class ShooterShip extends MotionShip {
 }
 
 // Override the existing global ship.
-Ship = ShooterShip
+Ship = BlasterShip
 
 let bulletFiringInterval;
 
@@ -129,3 +200,4 @@ document.addEventListener('keyup', (e) => {
         clearInterval(bulletFiringInterval);
     }
 });
+
